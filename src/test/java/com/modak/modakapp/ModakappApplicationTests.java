4589397541530559000a31212b7f2d9b7f41b108
맹순영 <symaeng98@naver.com>
2022-10-18 12:16:38 +0900

    feat: profile test

--- src/test/java/com/modak/modakapp/ModakappApplicationTests.java
+++ src/test/java/com/modak/modakapp/ModakappApplicationTests.java
@@ -1,14 +1,13 @@
 package com.modak.modakapp;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class ModakappApplicationTests {
 
-	@Test
-	void contextLoads() {
+    @Test
+    void contextLoads() {
 
-	}
+    }
 
 }
