f5052228f70f0f738699823e4caf22686c069fd5
맹순영 <symaeng98@naver.com>
2022-10-19 18:00:16 +0900

    feat: 배포 테스트

--- src/main/java/com/modak/modakapp/ModakappApplication.java
+++ src/main/java/com/modak/modakapp/ModakappApplication.java
@@ -11,7 +11,5 @@ public class ModakappApplication {
     public static void main(String[] args) {
         SpringApplication.run(ModakappApplication.class, args);
         System.out.println("배포 테스트입니다.");
-        System.out.println("배포 테스트입니다2");
-        System.out.println("배포 테스트입니다3");
     }
 }
