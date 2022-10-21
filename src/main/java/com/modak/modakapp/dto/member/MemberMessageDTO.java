b0ad17db4056805acde9bbb32e6bcc85095bd2fe
맹순영 <symaeng98@naver.com>
2022-10-21 15:59:46 +0900

    fix: 채팅 metadata 수정

--- src/main/java/com/modak/modakapp/dto/member/MemberMessageDTO.java
+++ src/main/java/com/modak/modakapp/dto/member/MemberMessageDTO.java
@@ -1,12 +1,10 @@
 package com.modak.modakapp.dto.member;
 
-import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-@Builder
 public class MemberMessageDTO {
     private String name;
     private String color;
